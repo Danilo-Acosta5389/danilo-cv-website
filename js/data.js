359aeac35aa85da8cdb478800ad3d98461a952d5
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-03-18 03:56:01 +0100

    Close nav menu

--- js/data.js
+++ js/data.js
@@ -184,3 +184,25 @@ window.addEventListener('click', (event) => {
         modalContactMe.classList.remove('show');
     }
 });
+
+
+
+/* ------------------- Modal nav menu ----------------*/
+
+const checkbox = document.getElementById('menu-toggle');
+const navModal = document.getElementById('top-nav-modal');
+
+checkbox.addEventListener('click', () => {
+  if (checkbox.checked == true) {
+  navModal.classList.add('show2')
+}
+else 
+navModal.classList.remove('show2')
+})
+
+window.addEventListener('click', (event) => {
+  if (event.target == navModal) {
+    navModal.classList.remove('show2');
+    checkbox.checked = false;
+  }
+})
