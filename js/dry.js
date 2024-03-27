74612a879217cf0849202668ca25e9e5a3d38ca8
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-03-27 16:18:59 +0100

    new links and icons

---
+++ js/dry.js
@@ -0,0 +1,7 @@
+document.getElementById("github").onclick = function () {
+        window.open("https://github.com/Danilo-Acosta5389", "_blank");
+    };
+
+document.getElementById("linkedin").onclick = function () {
+        window.open("https://www.linkedin.com/in/danilo-acosta-dote-208a3224b/", "_blank");
+    };
