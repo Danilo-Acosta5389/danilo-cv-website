3eea2b64ad2b9487007e2f432cd987725aadaa35
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-03-28 14:36:03 +0100

    css and new video

--- js/about.js
+++ js/about.js
@@ -21,20 +21,20 @@ window.addEventListener('click', (event) => {
 
 /* ------------------- Modal nav menu ----------------*/
 
-const checkbox = document.getElementById('menu-toggle');
-const navModal = document.getElementById('top-nav-modal');
+// const checkbox = document.getElementById('menu-toggle');
+// const navModal = document.getElementById('top-nav-modal');
 
-checkbox.addEventListener('click', () => {
-  if (checkbox.checked == true) {
-  navModal.classList.add('show2')
-}
-else 
-navModal.classList.remove('show2')
-})
+// checkbox.addEventListener('click', () => {
+//   if (checkbox.checked == true) {
+//   navModal.classList.add('show2')
+// }
+// else 
+// navModal.classList.remove('show2')
+// })
 
-window.addEventListener('click', (event) => {
-  if (event.target == navModal) {
-    navModal.classList.remove('show2');
-    checkbox.checked = false;
-  }
-})
+// window.addEventListener('click', (event) => {
+//   if (event.target == navModal) {
+//     navModal.classList.remove('show2');
+//     checkbox.checked = false;
+//   }
+// })
--- js/dry.js
+++ js/dry.js
@@ -5,3 +5,24 @@ document.getElementById("github").onclick = function () {
 document.getElementById("linkedin").onclick = function () {
         window.open("https://www.linkedin.com/in/danilo-acosta-dote-208a3224b/", "_blank");
     };
+
+
+    /* ------------------- Modal nav menu ----------------*/
+
+const checkbox = document.getElementById('menu-toggle');
+const navModal = document.getElementById('top-nav-modal');
+
+checkbox.addEventListener('click', () => {
+  if (checkbox.checked == true) {
+  navModal.classList.add('show2')
+}
+else 
+navModal.classList.remove('show2')
+})
+
+window.addEventListener('click', (event) => {
+  if (event.target == navModal) {
+    navModal.classList.remove('show2');
+    checkbox.checked = false;
+  }
+})
